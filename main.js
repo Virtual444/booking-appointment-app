97eaa5438b6802cb950a0471d9150cb9f3b63b1f
asitpal211 <asitpal211@gmail.com>
2023-06-24 01:18:46 +0530

    added edit functionality

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("BOOKING APPOINTMENT APP");
 document.addEventListener("DOMContentLoaded", () => {
-    axios.get('https://crudcrud.com/api/521a558fd18041db9f89fc0255cdba72/appointmentData')
+    axios.get('https://crudcrud.com/api/e24f96989d9d408ca1883386a4d3bc4d/appointmentData')
     .then(response => {
         console.log(response);
         for(let i=0 ; i<response.data.length ; i++){
@@ -21,7 +21,7 @@ function addUser(){
         email: email,
         phone: phone
     }
-    axios.post('https://crudcrud.com/api/521a558fd18041db9f89fc0255cdba72/appointmentData', userDetails)
+    axios.post('https://crudcrud.com/api/e24f96989d9d408ca1883386a4d3bc4d/appointmentData', userDetails)
     .then(response => {
         console.log(response);
     })
@@ -40,7 +40,11 @@ function displayUserOnScreen(userObj){
     const button = document.createElement('button');
     button.textContent = "DELETE";
     button.onclick = deleteUser;
+    const button1 = document.createElement('button');
+    button1.textContent = "EDIT";
+    button1.onclick = () => editUser(userObj._id, userObj.name, userObj.email, userObj.phone);
 
+    li.appendChild(button1);
     li.appendChild(button)
 
     parentElement.appendChild(li);
@@ -50,7 +54,7 @@ function deleteUser(event){
     let id = event.target.parentElement.id;
     console.log("ID OF THIS LI ", id);
 
-    axios.delete(`https://crudcrud.com/api/521a558fd18041db9f89fc0255cdba72/appointmentData/${id}`)
+    axios.delete(`https://crudcrud.com/api/e24f96989d9d408ca1883386a4d3bc4d/appointmentData/${id}`)
     .then(res => {
         console.log(res);
         location.reload();
@@ -59,3 +63,42 @@ function deleteUser(event){
         console.log("Error while deleting", err);
     })
 }
+
+
+function editUser(id, n, e, p) {
+
+    const button = document.getElementById('submit');
+    button.textContent = "UPDATE" ;
+    button.onclick = updateUser
+    // const li = event.target.parentElement;
+    // const id = li.id;
+    document.getElementById('name').value = n;
+    document.getElementById('email').value = e;
+    document.getElementById('phone').value = p;
+
+    function updateUser(){
+        const updatedName = document.getElementById('name').value;
+        const updatedEmail = document.getElementById('email').value;
+        const updatedPhone = document.getElementById('phone').value;
+
+        const updatedUserDetails = {
+            name: updatedName,
+            email: updatedEmail,
+            phone: updatedPhone
+        }
+
+        axios.put(`https://crudcrud.com/api/e24f96989d9d408ca1883386a4d3bc4d/appointmentData/${id}`, updatedUserDetails)
+        .then(response => {
+            console.log(response);
+            location.reload();
+        })
+        .catch(err => {
+            console.log("error in updation", err);
+        })
+
+    }
+
+    
+
+    
+  }
